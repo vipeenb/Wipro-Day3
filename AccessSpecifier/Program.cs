b3311061f60468f5d67c9146dda3702c250bf28b
vipeenb <vipeen.bendale@gmail.com>
2025-01-29 13:28:25 +0530

    Day3

---
+++ AccessSpecifier/Program.cs
@@ -0,0 +1,15 @@
+﻿using WiproDay3;
+
+namespace AccessSpecifier
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Customer cst = new Customer();
+            cst.custId = 101;
+            cst.custName = "Vipeen";
+            cst.Display();
+        }
+    }
+}
---
+++ WiproDay3/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiproDay3
+{
+    public class Customer
+    {
+        public int custId;
+        public string custName;
+
+        public void Display()
+        {
+            Console.WriteLine($"CustomerId:{custId} \n CustomerName: {custName}");
+        }
+    }
+}
